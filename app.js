56f53594bb1cf9c8e9f4dfad892d463bcc5ce4e5
Maksym Pavlenko <32528661+maxpavlenko@users.noreply.github.com>
2019-09-05 22:37:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const kelvin = 10;
 //Convert Kelvin to Celsius
 const celsius = kelvin - 273;
 
-//Convert Kelvin to Fahrenheit
+//Convert to Fahrenheit
 let fahrenheit = celsius * (9 / 5) + 32;
 
 //Round down the Fahrenheit 
@@ -13,7 +13,11 @@ fahrenheit = Math.floor(fahrenheit);
 //Print convert result
 console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit.');
 
-//Convert Kelvin to Celsius
+//Convert to Celsius
 let newton = celsius * (33 / 100);
+
+//Round down the Newton
 newton = Math.floor(newton);
+
+//Print convert result
 console.log(`The temperature is ${newton} degrees Newton.`);
